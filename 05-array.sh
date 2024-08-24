5668de3eabc78a545623e0ad846eb30ee6c01b5a
srinivasu dandamudi <srinivasd2mail@gmail.com>
2024-08-24 13:55:12 +0800

    variables correct

--- 05-array.sh
+++ 05-array.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-FRUITS=("APPLE","KIWI","ORANGE")
+FRUITS=("APPLE" "KIWI" "ORANGE")
 
 echo "First fruit is: ${FRUITS[0]}"
 
+echo "Second fruit is: ${FRUITS[1]}"
 
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "First  fruit is: ${FRUITS[@]}"
 
