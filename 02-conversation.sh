a99ccb271e88ecc8b9b8bacdc1d3cb2f2ba7c113
srinivasu dandamudi <srinivasd2mail@gmail.com>
2024-08-24 13:20:10 +0800

    added variables to the second bash script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PERSON1 =Ramesh
+PERSON2 =Suresh
 
-
-echo "Ramesh::Hi Suresh, How are you ?"
-echo "Suresh:: Hello Ramesh.I am fine.How are you doing ?"
-echo "Ramesh:: I am doing good suresh. What's going on?"
-echo "Suresh:: I started learning shell script Ramesh"
+echo "${PERSON1}::Hi ${PERSON2}, How are you ?"
+echo "${PERSON2}:: Hello ${PERSON1}.I am fine.How are you doing ?"
+echo "${PERSON1}:: I am doing good ${PERSON2}. What's going on?"
+echo "${PERSON2}:: I started learning shell script ${PERSON1}"
