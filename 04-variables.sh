03e08db738d992d502fc486cba7766c20a3119e1
srinivasu dandamudi <srinivasd2mail@gmail.com>
2024-08-24 13:41:20 +0800

    variables correct

---
+++ 04-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter your username"
+
+read USERNAME
+
+echo "your username is $USERNAME " 
+
+
+echo "enter your password"
+
+read PASSWORD
+
+echo "your username is $USERNAME"
+
+echo "your password is $PASSWORD"
