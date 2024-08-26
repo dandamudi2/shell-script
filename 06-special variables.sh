65ac310942881cb67b7bbba4c28c02c4b802a5ad
srinivasu dandamudi <srinivasd2mail@gmail.com>
2024-08-26 10:12:55 +0800

    special variables

---
+++ 06-special variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Total variables passed to the script $@"
+
+echo "Number of variables passed: $#"
+echo "Script name : $0"
+echo "Current working directory :$PWD" 
+echo "Home directory of current user : $HOME"
+echo "ID Of the script executing now : $$"
+
+
+ 
