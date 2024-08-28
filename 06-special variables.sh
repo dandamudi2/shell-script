b003744a827d90c809dfb7329a6bd2e0325877db
srinivasu dandamudi <srinivasd2mail@gmail.com>
2024-08-28 22:29:46 +0800

    adding commands

--- 06-special variables.sh
+++ 06-special variables.sh
@@ -2,7 +2,7 @@
 
 echo "All variables passed to the script $@"
 
-# echo "Number of variables passed: $#"
+ echo "Number of variables passed to the script: $#"
 # echo "Script name : $0"
 # echo "Current working directory :$PWD" 
 # echo "Home directory of current user : $HOME"
