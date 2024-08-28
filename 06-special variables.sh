a326b123f28b6a671b287f50db219e4a3e339ff6
srinivasu dandamudi <srinivasd2mail@gmail.com>
2024-08-28 22:28:04 +0800

    adding commands

--- 06-special variables.sh
+++ 06-special variables.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Total variables passed to the script $@"
+echo "All variables passed to the script $@"
 
-echo "Number of variables passed: $#"
-echo "Script name : $0"
-echo "Current working directory :$PWD" 
-echo "Home directory of current user : $HOME"
-echo "ID Of the script executing now : $$"
+# echo "Number of variables passed: $#"
+# echo "Script name : $0"
+# echo "Current working directory :$PWD" 
+# echo "Home directory of current user : $HOME"
+# echo "ID Of the script executing now : $$"
 
-sleep 100 & 
+# sleep 100 & 
 
-echo "PD of last background command : $!"
+# echo "PD of last background command : $!"
 
 
  
